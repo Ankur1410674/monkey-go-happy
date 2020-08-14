e4e9f8c71344004521b945e5be8508695407b10b
Ankur1410674 <69246028+Ankur1410674@users.noreply.github.com>
2020-08-14 17:13:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var player,banana,stone,jungle;
+
+
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  
+    player = createSprite(50,180,20,50);
+ 
+  
+  
+}
+
+function draw() {
+  background(220);
+  drawSprites()
+}
